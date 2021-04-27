c85a7a1deb2d49d5865ba1f8237ff535237e5eee
Adam <w4rr3d@gmail.com>
2021-04-27 16:57:06 +0200

    ajout @CrossOrigin pour @get sans token

--- src/main/java/demo/controller/UtilisateurController.java
+++ src/main/java/demo/controller/UtilisateurController.java
@@ -3,6 +3,7 @@ package demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import demo.model.Utilisateur;
 import demo.service.IUtilisateur;
 
 @RestController
+@CrossOrigin
 public class UtilisateurController {
 	
 	@Autowired
